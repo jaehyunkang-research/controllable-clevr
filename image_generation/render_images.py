ae94a1252d70edf87f1f14e0426842d01eeffada
jaehyunkang-research <youkind@yonsei.ac.kr>
2023-04-08 18:22:49 +0000

    add render_from_json

--- image_generation/render_images.py
+++ image_generation/render_images.py
@@ -386,6 +386,176 @@ def render_scene_deterministic(
 
   return scene_struct
 
+def render_from_json(
+    args,    
+    output_index=0,
+    output_split='test',
+    output_dir='output',
+    json_file=None, 
+    json_path=None, 
+    aug=None):
+
+  assert json_file or json_path, "Either json_file or json_path must be specified"
+
+  if json_file is None:
+      with open(json_path, 'r') as f:
+        scene_struct = json.load(f)
+  else:
+    scene_struct = json_file
+
+  # Load the main blendfile
+  bpy.ops.wm.open_mainfile(filepath=args.base_scene_blendfile)
+  # Load materials
+  utils.load_materials(args.material_dir)
+
+  # Set render arguments so we can get pixel coordinates later.
+  # We use functionality specific to the CYCLES renderer so BLENDER_RENDER
+  # cannot be used.
+  output_scene = os.path.join(*[output_dir, 'aug', 'scenes', str(output_index).zfill(4)+'.json'])
+  output_image = os.path.join(*[output_dir, 'aug', 'images', str(output_index).zfill(4)+'.png'])
+  render_args = bpy.context.scene.render
+  render_args.engine = "CYCLES"
+  render_args.filepath = output_image
+  render_args.resolution_x = args.width
+  render_args.resolution_y = args.height
+  render_args.resolution_percentage = 100
+  render_args.tile_x = args.render_tile_size
+  render_args.tile_y = args.render_tile_size
+  if args.use_gpu == 1:
+    # Blender changed the API for enabling CUDA at some point
+    if bpy.app.version < (2, 78, 0):
+      bpy.context.user_preferences.system.compute_device_type = 'CUDA'
+      bpy.context.user_preferences.system.compute_device = 'CUDA_0'
+    else:
+      cycles_prefs = bpy.context.user_preferences.addons['cycles'].preferences
+      cycles_prefs.compute_device_type = 'CUDA'
+
+  # Some CYCLES-specific stuff
+  bpy.data.worlds['World'].cycles.sample_as_light = True
+  bpy.context.scene.cycles.blur_glossy = 2.0
+  bpy.context.scene.cycles.samples = args.render_num_samples
+  bpy.context.scene.cycles.transparent_min_bounces = args.render_min_bounces
+  bpy.context.scene.cycles.transparent_max_bounces = args.render_max_bounces
+  if args.use_gpu == 1:
+    bpy.context.scene.cycles.device = 'GPU'
+
+  # Put a plane on the ground so we can compute cardinal directions
+  bpy.ops.mesh.primitive_plane_add(radius=5)
+  plane = bpy.context.object
+
+  # Figure out the left, up, and behind directions along the plane and record
+  # them in the scene structure
+  camera = bpy.data.objects['Camera']
+  plane_normal = plane.data.vertices[0].normal
+  cam_behind = camera.matrix_world.to_quaternion() * Vector((0, 0, -1))
+  cam_left = camera.matrix_world.to_quaternion() * Vector((-1, 0, 0))
+  cam_up = camera.matrix_world.to_quaternion() * Vector((0, 1, 0))
+  plane_behind = (cam_behind - cam_behind.project(plane_normal)).normalized()
+  plane_left = (cam_left - cam_left.project(plane_normal)).normalized()
+  plane_up = cam_up.project(plane_normal).normalized()
+
+  # Delete the plane; we only used it for normals anyway. The base scene file
+  # contains the actual ground plane.
+  utils.delete_object(plane)
+
+  # Save all six axis-aligned directions in the scene struct
+  scene_struct['directions']['behind'] = tuple(plane_behind)
+  scene_struct['directions']['front'] = tuple(-plane_behind)
+  scene_struct['directions']['left'] = tuple(plane_left)
+  scene_struct['directions']['right'] = tuple(-plane_left)
+  scene_struct['directions']['above'] = tuple(plane_up)
+  scene_struct['directions']['below'] = tuple(-plane_up)
+
+
+  ###
+  color_name_to_rgba = {
+              'purple': [0.5058823529411764, 0.14901960784313725, 0.7529411764705882, 1.0], 
+              'brown': [0.5058823529411764, 0.2901960784313726, 0.09803921568627451, 1.0], 
+              'blue': [0.16470588235294117, 0.29411764705882354, 0.8431372549019608, 1.0], 
+              'gray': [0.3411764705882353, 0.3411764705882353, 0.3411764705882353, 1.0], 
+              'green': [0.11372549019607843, 0.4117647058823529, 0.0784313725490196, 1.0], 
+              'red': [0.6784313725490196, 0.13725490196078433, 0.13725490196078433, 1.0], 
+              'yellow': [1.0, 0.9333333333333333, 0.2, 1.0], 
+              'cyan': [0.1607843137254902, 0.8156862745098039, 0.8156862745098039, 1.0]}
+  shape2name = {'cylinder' : 'SmoothCylinder', 'cube' : 'SmoothCube_v2', 'sphere' : 'Sphere'}
+  mat2name = {'rubber' : 'Rubber', 'metal' : 'MyMetal'}
+  
+  objects = []
+  blender_objects = []
+
+  assert len(scene_struct['objects']) == len(aug), "Object number mismatch"
+  for i, object_info in enumerate(scene_struct['objects']):
+    # print(object_info)
+    color = object_info['color']
+    coords_pixel = object_info['pixel_coords']
+    coords_3d = object_info['3d_coords']
+    size = object_info['size']
+    material = object_info['material']
+    shape = object_info['shape']
+    rotation = object_info['rotation']
+    # rgba = color_name_to_rgba[color]
+    rgba = color
+    size_num = coords_3d[2]
+    (x,y) = coords_3d[0:2]
+
+    
+
+    # apply augmentations
+    if aug is not None:
+      aug_params = aug[i]
+      dx, dy = aug_params['trans']
+      dz = aug_params['size']
+      
+      x += dx
+      y += dy
+      size_num *= dz
+
+    utils.add_object(args.shape_dir, shape2name[shape], size_num, (x, y), theta=rotation)
+    obj = bpy.context.object
+    blender_objects.append(obj)
+    pixel_coords = utils.get_camera_coords(camera, obj.location)
+    objects.append({
+      'shape': shape,
+      'size': size_num,
+      'material': material,
+      '3d_coords': tuple(obj.location),
+      'rotation': rotation,
+      'pixel_coords': pixel_coords,
+      'color': rgba,
+    })
+
+    utils.add_material(mat2name[material], Color=rgba)
+
+  scene_struct['objects'] = objects
+  scene_struct['relationships'] = compute_all_relationships(scene_struct)
+
+  while True:
+    try:
+      bpy.ops.render.render(write_still=True)
+      break
+    except Exception as e:
+      print(e)
+
+  # makedir
+  image_dir = os.path.join(*[output_dir, 'aug', 'images'])
+  scene_dir = os.path.join(*[output_dir, 'aug', 'scenes'])
+  mask_dir = os.path.join(*[output_dir, 'aug', 'masks'])
+
+  os.makedirs(image_dir, exist_ok=True)
+  os.makedirs(scene_dir, exist_ok=True)
+  os.makedirs(mask_dir, exist_ok=True)
+
+  with open(output_scene, 'w') as f:
+    json.dump(scene_struct, f, indent=2)
+
+  for i, o in enumerate(blender_objects):
+    path = os.path.join(mask_dir, str(output_index).zfill(4) + '_' + str(i) + '.png')
+    render_mask([o], path, whole=False)
+    
+  path = os.path.join(mask_dir, str(output_index).zfill(4) + '_0.png')
+  render_mask(blender_objects, path, whole=True)
+
+
 def render_scene(args,
     num_objects=5,
     output_index=0,
